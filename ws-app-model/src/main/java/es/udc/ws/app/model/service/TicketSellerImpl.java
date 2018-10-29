72c34b88041fc35d13b2ddab628481f33cd5dfec
Alejandro Romero Rivera <alejandro.romero.rivera@udc.es>
2018-10-29 09:58:29 +0100

    TicketSellerService constructor created

---
+++ ws-app-model/src/main/java/es/udc/ws/app/model/service/TicketSellerImpl.java
@@ -0,0 +1,60 @@
+package es.udc.ws.app.model.service;
+
+import es.udc.ws.app.model.reservation.Reservation;
+import es.udc.ws.app.model.reservation.SqlReservationDao;
+import es.udc.ws.app.model.reservation.SqlReservationDaoFactory;
+import es.udc.ws.app.model.service.exceptions.LimitDateExceeded;
+import es.udc.ws.app.model.service.exceptions.ReservationAlreadyChecked;
+import es.udc.ws.app.model.show.Show;
+import es.udc.ws.app.model.show.SqlShowDao;
+import es.udc.ws.app.model.show.SqlShowDaoFactory;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import es.udc.ws.util.sql.DataSourceLocator;
+
+import javax.sql.DataSource;
+import java.util.Calendar;
+import java.util.List;
+
+public class TicketSellerImpl implements TicketSellerService
+{
+    private final DataSource dataSource;
+    private SqlReservationDao reservationDao;
+    private SqlShowDao showDao;
+
+    public TicketSellerImpl() {
+        dataSource = DataSourceLocator.getDataSource("ws-javaexamples-ds");
+        reservationDao = SqlReservationDaoFactory.getDao();
+        showDao = SqlShowDaoFactory.getDao();
+    }
+
+    @Override
+    public Show createShow(Show show) throws InputValidationException {
+        return null;
+    }
+
+    @Override
+    public Show findShow(String code) throws InstanceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Show> findShows(String words, Calendar start, Calendar end) {
+        return null;
+    }
+
+    @Override
+    public Reservation bookTickets(Long showId, String email, String cardNumber, int count) throws LimitDateExceeded {
+        return null;
+    }
+
+    @Override
+    public List<Reservation> getUserReservations(String email) {
+        return null;
+    }
+
+    @Override
+    public void checkReservation(String code) throws InstanceNotFoundException, ReservationAlreadyChecked {
+
+    }
+}
